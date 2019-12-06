5b0402e7ea956b607b7c5aba12f9f389d012f7ad
xxks-kkk <ferrishu3886@gmail.com>
2019-12-06 10:37:41 -0600

    Finish lexing

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
-package interpreter
+package main
+
+import (
+	"fmt"
+	"github.com/xxks-kkk/interpreter/repl"
+	"os"
+	"os/user"
+)
 
 func main() {
-
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the little programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
