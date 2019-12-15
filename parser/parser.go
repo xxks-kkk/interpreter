1f75bdc1b578b8916af4373cf344d2f31582cd79
xxks-kkk <ferrishu3886@gmail.com>
2019-12-14 20:18:28 -0600

    reformat

--- parser/parser.go
+++ parser/parser.go
@@ -1,31 +1,32 @@
 package parser
 
-import "github.com/xxks-kkk/interpreter/token"
-import "github.com/xxks-kkk/interpreter/lexer"
-import "github.com/xxks-kkk/interpreter/ast"
+import (
+	"github.com/xxks-kkk/interpreter/ast"
+	"github.com/xxks-kkk/interpreter/lexer"
+	"github.com/xxks-kkk/interpreter/token"
+)
 
 type Parser struct {
-     l *lexer.Lexer
-     curToken token.Token
-     peekToken token.Token
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-     p := &Parser{l: l}
+	p := &Parser{l: l}
 
-     // Read two tokens, so curToken and peekToken are both set
-     p.nextToken()
-     p.nextToken()
+	// Read two tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
 
-     return p
+	return p
 }
 
 func (p *Parser) nextToken() {
-     p.curToken = p.peekToken
-     p.peekToken = p.l.NextToken()
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-     return nil
+	return nil
 }
-
